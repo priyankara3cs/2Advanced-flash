cedde5742c217eff0dcb155fb2d2b459456f7a4f
priyankara3cs <priyankara@3cs.solutions>
2024-09-28 12:09:37 +0530

    update the page 2

--- src/components/page-2.js
+++ src/components/page-2.js
@@ -1,9 +1,84 @@
-import React from 'react'
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "./ImageGallery.css";
 
-export default function Page() {
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// Import required modules from 'swiper/modules'
+import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
+
+import slide_image_1 from "../assets/img_1.jpg";
+import slide_image_2 from "../assets/img_2.jpg";
+import slide_image_3 from "../assets/img_3.jpg";
+import slide_image_4 from "../assets/img_4.jpg";
+import slide_image_5 from "../assets/img_5.jpg";
+import slide_image_6 from "../assets/img_6.jpg";
+import slide_image_7 from "../assets/img_7.jpg";
+
+function App() {
   return (
-    <div style={{ padding: "20px" }}>
-      <div>page-2</div>
+    <div className="scrollable-container">
+      <div className="container">
+        <Swiper
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          loop={true}
+          slidesPerView={"auto"}
+          coverflowEffect={{
+            rotate: 0,
+            stretch: 0,
+            depth: 100,
+            modifier: 2.5,
+          }}
+          pagination={{ el: ".swiper-pagination", clickable: true }}
+          navigation={{
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+            clickable: true,
+          }}
+          modules={[EffectCoverflow, Pagination, Navigation]}
+          className="swiper_container"
+        >
+          <SwiperSlide>
+            <img src={slide_image_1} alt="slide_image" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slide_image_2} alt="slide_image" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slide_image_3} alt="slide_image" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slide_image_4} alt="slide_image" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slide_image_5} alt="slide_image" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slide_image_6} alt="slide_image" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={slide_image_7} alt="slide_image" />
+          </SwiperSlide>
+
+          <div className="slider-controler">
+            <div className="swiper-button-prev slider-arrow">
+              <ion-icon name="arrow-back-outline"></ion-icon>
+            </div>
+            <div className="swiper-button-next slider-arrow">
+              <ion-icon name="arrow-forward-outline"></ion-icon>
+            </div>
+            <div className="swiper-pagination"></div>
+          </div>
+        </Swiper>
+      </div>
     </div>
   );
 }
+
+export default App;
